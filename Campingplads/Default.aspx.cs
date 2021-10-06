cdb3c7b8c509bd07d1ddfe539c48deda302a6da1
Carlo2 <zbccmvm@ZBC-E-RO-23246>
2021-10-06 09:02:13 +0200

    Backend first batch

--- Campingplads/Default.aspx.cs
+++ Campingplads/Default.aspx.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Campingplads.DataControl;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,8 +11,26 @@ namespace Campingplads
 {
     public partial class _Default : Page
     {
+        DatabaseInterface dbInterface;
         protected void Page_Load(object sender, EventArgs e)
         {
+            dbInterface = new DatabaseInterface();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("click");
+        }
+
+        protected void btnClick(object sender, EventArgs e)
+        {
+            //List<CampingSpotTypeInformation> list = dbInterface.GetAvaibleSpotTypesInDates(new DateTime(2021, 11, 03), new DateTime(2021, 11, 15));
+
+            //foreach(CampingSpotTypeInformation m in list)
+            //{
+            //    Debug.WriteLine("");
+            //    Debug.Write($"{m.SpotName} - {m.SpotType} - {m.SpotDescription} - {m.SquareMeters} - {m.MaxPeople}");
+            //}
 
         }
     }
---
+++ Campingplads/LogicControl/DataProcessor.cs
@@ -0,0 +1,27 @@
+﻿using Campingplads.DataControl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Campingplads.LogicControl
+{
+    public class DataProcessor
+    {
+        private DatabaseInterface _dbInterface;
+
+        public DataProcessor()
+        {
+            _dbInterface = new DatabaseInterface();
+        }
+
+        public CampingSpotTypeInformation GetSpotTypeInformation(int spotType)
+        {
+            return _dbInterface.GetCampingSpotTypeInformation(spotType);
+        }
+
+
+    }
+
+
+}
